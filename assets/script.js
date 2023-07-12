58ce1bf6d52243afe175a90064e84e2d8d264a1a
frausto98 <frausto9298@yahoo.com>
2023-07-11 17:47:56 -0700

    finished js code

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-    // each todo is a function
-    //additino to the todos: set interval ti update the time throughout the day
-    //event listener towards the end of the files
-    //function call to start the app
-
-
-
 $(function () {
     // TODO: Add a listener for click events on all save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -19,26 +12,21 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
 
-        // add event listener for the save buttons
-
-        //$(".saveBtn").on('click', (){
-
-        //})
-        //console.log($(this));
-
-        //figure out what block we're in using $(this)
-        // key names need to match the key names for getItem localStorage
-        // save the item to local storage when user clicks on the save button
-            // efficiently, only save that particular hour, use keyword "this" for function
-
-
-
-        // use the id as the ley to save the text to the key
         // create a ".on('click', ___)"
-        // create two variables, one for the ID, and one for the textContent
-          //textContent is from user input value
-        // target the save button, save it to the localStorage or 
+        // create two variables, one for the ID, and one for the content
+          //content is from user input value
+        // target the save button, save it to the localStorage
+        $(".saveBtn").on('click', function(){
+            // figure out what block we're in using $(this)
+            // seperate specific key in localStorage for each hour - whatever you want to name it
+            var key =$(this).parent().attr('id')
+            // select the text area to save for the corresponding save button
+            var value = $(this).siblings('textarea').val()
+            // save the item to local storage when user clicks on the save button
+            // efficiently, only save that particular hour, use keyword "this" for function
+            localStorage.setItem(key, value)
 
+        })
 
     // First *******************
     // TODO: Add code to apply the past, present, or future class to each time
@@ -47,45 +35,55 @@ $(function () {
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     //
-    //should be 1 time code
 
         // jquery .children to select children elements as an array
-        //document.querySelectorAll('.time-block') // return an array of all time blcok names and can be used for the for loop (jquery)
+        // document.querySelectorAll('.time-block') // return an array of all time blcok names and can be used for the for loop (jquery)
+        // we need to know current hour
 
-        // create a var that will save the current time and use day.js (just the hour) dayjs('hh')
-        // LOOP over the time blocks
-            // inside the for loop select the .time-block
-            
-            //for(i = 9; i < 19; i++){
-            //document.querySelector('#hour-' + i) //jquery method
-            //}
-            // CONDITIONAL compare the block hour and the current hour 
-                // setAttribute the style based on what day.js returns/is currently
+        function updateGrid() {
+
+            var allHourBlocks = $('.time-block')
+            var currentHour = dayjs().hour(); //maybe format() instead of hour, military time
+            allHourBlocks.each(function(){
+                // the var is assigned to  whatever THIS container element is that is being targeted with an id, and is splitting the string of the id "hour-x" right at the '-'
+                var currentIndex = $(this).attr('id').split('-')[1];
+
+
+                // CONDITIONAL compare the block hour and the current hour 
+                // setAttribute the style based on what day.js returns/is currently ********** use i instead
                     // if current HOUR is LESS THAN, apply future
                     // else if current HOUR is EQUAL to current time, apply present
                     // else current HOUR is MORE THAN, apply past
-        // use append to attach the Past Present and Future classes to designated boxes
 
-        // bonus: have the time auto refresh every other minute using setInterval
-        
+                if ( currentIndex < currentHour ) {
+                    $(this).addClass('past');
+                }
+                else if ( currentIndex == currentHour ) {
+                    $(this).addClass('present');
+                } 
+                else  {
+                    $(this).addClass('future');
+                }
 
+            });
+
+            
+
+        }
+
+        updateGrid();
 
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
-    //should be 1 time code
-        
         // getItem to check & get the user's OLD input get each content by ID, if the ID has an input/is not null, it will have something in the localStorage
-            // seperate specific key in localStorage for each hour - whatever you want to name it
-            // another for loop starting from hour 9 to hour 18
-                // check local storage if !null, display items 
-                    //inside the text area [1] of the corresponding hour
-                        //use id selector
-
-        // and use the variables assigned to textContent
-        // create a for statement ?????
-
+        // for loop starting from hour 9 to hour 18
+        for (var i = 9; i < 20; i++) {
+        // target the all with id hour-, with looping thru the index, the puts  into the text area
+        // seperate specific key in localStorage for each hour - whatever you want to name it
+        $('#hour-' + i + ' textarea').val(localStorage.getItem('hour-' + i))
+        }
 
     // TODO: Add code to display the current date in the header of the page.
 
